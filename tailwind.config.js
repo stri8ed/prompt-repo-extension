a8e48d263bf427896170bddc7328b312a6396509
Levi Melamed <levi@levimelamed.com>
2024-10-29 17:18:54 -0500

    fix CSP error https://github.com/crxjs/chrome-extension-tools/commit/f4eb4d496550491e339393cc03678e4ffbe99be2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography'
+
 export default {
     content: [
         "./src/**/*.{html,js,ts,jsx,tsx}",
@@ -8,6 +10,6 @@ export default {
         },
     },
     plugins: [
-        require('@tailwindcss/typography'),
+        typography,
     ],
 }
